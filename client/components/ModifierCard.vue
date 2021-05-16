d690b8b2a3917c30162f0b0ec4a5b6534074d7a9
Romitou <romitou@protonmail.com>
2021-05-16 14:34:49 +0200

    :bento: Update material assets

--- client/components/ModifierCard.vue
+++ client/components/ModifierCard.vue
@@ -1,8 +1,8 @@
 <template>
   <li class="col-span-1 bg-gray-700 border border-gray-500 rounded shadow divide-y divide-gray-200">
     <div class="w-full flex items-center justify-between">
-      <img class="h-24 p-2 ml-2 w-auto rounded rounded-r-none" :src="require(`~/assets/${modifier.material.toLowerCase()}.png`)" :alt="modifier.material">
-      <div class="flex-1 truncate p-6">
+      <img class="h-24 p-2 m-4 w-auto" :src="require(`~/assets/${modifier.material.toLowerCase()}.png`)" :alt="modifier.material">
+      <div class="flex-1 truncate py-6">
         <div class="flex items-center space-x-1">
           <h3 class="text-white font-medium truncate mr-2">
             {{ modifier.name }}
