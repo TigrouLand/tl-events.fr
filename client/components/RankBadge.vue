649eb0c4870a54391096edc6dfebcec92a24834b
Romitou <romitou@protonmail.com>
2021-05-25 19:38:58 +0200

    :art: Add builder and graphist badges

--- client/components/RankBadge.vue
+++ client/components/RankBadge.vue
@@ -2,6 +2,12 @@
   <div v-if="rank === 'Admin'">
     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-red-400 rounded-full">Admin</span>
   </div>
+  <div v-else-if="rank === 'Builder'">
+    <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-blue-400 rounded-full">Builder</span>
+  </div>
+  <div v-else-if="rank === 'Graphist'">
+    <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-yellow-400 rounded-full">Graphiste</span>
+  </div>
   <div v-else>
     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-gray-400 rounded-full">Joueur</span>
   </div>
