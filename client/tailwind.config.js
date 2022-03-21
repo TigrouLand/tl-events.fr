20fb915e31d4fe5c5b311b0d48bce552d9e1bad8
Romitou <romitou@protonmail.com>
2022-03-21 21:37:08 +0100

    :lipstick: Update TailwindCSS config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
   theme: {
     extend: {
       colors: {
