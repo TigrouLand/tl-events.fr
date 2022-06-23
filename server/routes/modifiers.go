df602b222a92710cc74ead548c5b60b73b8e3c07
Romitou <romitou@protonmail.com>
2022-06-23 19:30:51 +0200

    :sparkles: Sort modifiers by field `slot`

--- server/routes/modifiers.go
+++ server/routes/modifiers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/tigrouland/eventsstats/server/mongo"
 	"github.com/tigrouland/eventsstats/server/mongo/entities"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"time"
 )
@@ -13,7 +14,7 @@ import (
 func Modifiers(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	cursor, err := mongo.Get().Collection("modifiers").Find(ctx, bson.D{})
+	cursor, err := mongo.Get().Collection("modifiers").Find(ctx, bson.D{}, options.Find().SetSort(bson.D{{"slot", 1}}))
 	if err != nil {
 		log.Fatal("an error occurred while retrieving the modifiers: ", err)
 	}
