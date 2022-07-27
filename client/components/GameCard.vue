6448dd3b173832074b23e3a631643c3ce3c99905
Romitou <romitou@protonmail.com>
2022-07-27 19:50:01 +0200

    :lipstick: Improve UI of game cards

--- client/components/GameCard.vue
+++ client/components/GameCard.vue
@@ -3,48 +3,58 @@
     <div class="w-full p-2">
       <div class="flex">
         <div class="text-white font-medium">
-          <font-awesome-icon v-if="isScheduled(game)" :icon="faClock" class="text-gray-400" />
-          <font-awesome-icon v-else-if="isArchived(game)" :icon="faArchive" class="text-gray-400" />
+          <font-awesome-icon v-if="isUpcoming(game)" :icon="faClock" class="text-gray-400" />
+          <font-awesome-icon v-else-if="isReset(game)" :icon="faArchive" class="text-gray-400" />
           <font-awesome-icon v-else :icon="faGamepad" class="text-green-500" />
           {{ game.name }} [#{{ game.id }}]
         </div>
       </div>
-      <div class="py-1 -space-x-1 overflow-hidden justify-start text-left" v-if="game.players">
+      <div class="mt-2 -space-x-1 overflow-hidden justify-start text-left" v-if="game.players">
         <img v-for="player in game.players.slice(0, 7)" :key="player" class="inline-block h-6 w-6 rounded-full ring-2 ring-gray-800" :src="'https://cravatar.eu/helmavatar/' + player + '/96'" alt="">
       </div>
-      <div v-if="isScheduled(game)" class="text-gray-300 font-light text-sm justify-start text-left">
+      <div v-if="isReset(game)" class="mt-2 text-gray-300 font-light text-sm justify-start text-left">
         <font-awesome-icon :icon="faClock" /> {{ format(game.scheduleDate) }}
       </div>
-      <div v-else class="text-gray-300 font-light text-sm justify-start text-left">
+      <div v-else class="mt-2 text-gray-300 font-light text-sm justify-start text-left">
         <font-awesome-icon :icon="faClock" /> {{ format(game.archiveDate) }}
       </div>
+      <div v-if="isReset(game)" class="mt-2 text-gray-300 font-light text-sm justify-start text-left">
+        <font-awesome-icon :icon="faArrowCircleRight" /> Voir les détails
+      </div>
     </div>
-  </button>
+  </component>
 </template>
 
 <script>
-import { faArchive, faClock, faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { faArchive, faClock, faGamepad, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
 import moment from 'moment';
 
 moment.locale('fr');
 
 export default {
   name: 'GameCard',
-  props: ['game'],
+  props: ['game', 'selected'],
   computed: {
     faClock: () => faClock,
     faGamepad: () => faGamepad,
-    faArchive: () => faArchive
+    faArchive: () => faArchive,
+    faArrowCircleRight: () => faArrowCircleRight
   },
   methods: {
     isArchived(game) {
       return game.archiveDate !== -1;
     },
     format: (timestamp) => {
-      return moment(timestamp).fromNow();
+      if (!timestamp || timestamp === -1)
+        return 'À venir';
+      const formatted = moment(timestamp).fromNow();
+      return formatted.charAt(0).toUpperCase() + formatted.slice(1);
     },
-    isScheduled(game) {
-      return game.startDate === -1 && game.scheduleDate !== -1;
+    isReset(game) {
+      return game.status === 'RESET';
+    },
+    isUpcoming(game) {
+      return game.status === 'CREATED';
     }
   }
 };
