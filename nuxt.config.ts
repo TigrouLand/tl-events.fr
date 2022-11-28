5023fb363d3d6bf08a2ed5d159a4daaab06a831c
Romitou <romitou@protonmail.com>
2022-11-28 18:31:42 +0100

    :bug: Attempt to fix favicon not showing on legacy browsers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,14 @@ export default {
     head: {
       meta: metaTags,
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1'
+      viewport: 'width=device-width, initial-scale=1',
+      links: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        }
+      ]
     }
   },
   css: [
