6064bcc30e5020c0ddeaad31826e30eae257fedf
Romitou <romitou@protonmail.com>
2023-05-29 15:11:16 +0200

    :sparkles: Add Streamer rank badge

--- components/RankBadge.vue
+++ components/RankBadge.vue
@@ -8,6 +8,9 @@
   <div v-else-if="props.rank === 'Graphist'">
     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-yellow-400 rounded-full">Graphiste</span>
   </div>
+  <div v-else-if="props.rank === 'Live'">
+    <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-purple-400 rounded-full">Streamer</span>
+  </div>
   <div v-else>
     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-gray-400 rounded-full">Joueur</span>
   </div>
