d3c807debfbd3e2ca8f1326cc46c9dee6e099e6a
Romitou <romitou@protonmail.com>
2023-05-29 13:31:39 +0200

    :bug: Bring back brand icons

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,11 +1,12 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { defineNuxtPlugin } from '#app';
 // eslint-disable-next-line import/named
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 config.autoAddCss = false;
-library.add(fas);
+library.add(fas, fab);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon);
