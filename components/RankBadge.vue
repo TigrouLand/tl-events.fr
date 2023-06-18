9160ce9f1c06420a5ac5fc9e71231c699ad2f294
Giovanni <42092549+Matocolotoe@users.noreply.github.com>
2023-06-18 12:07:03 +0200

    🐛 Fix rank badges

--- components/RankBadge.vue
+++ components/RankBadge.vue
@@ -1,14 +1,14 @@
 <template>
-  <div v-if="props.rank === 'Admin'">
+  <div v-if="props.rank === 'ADMIN'">
     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-red-400 rounded-full">Admin</span>
   </div>
-  <div v-else-if="props.rank === 'Builder'">
+  <div v-else-if="props.rank === 'BUILDER'">
     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-blue-400 rounded-full">Builder</span>
   </div>
-  <div v-else-if="props.rank === 'Graphist'">
+  <div v-else-if="props.rank === 'GRAPHIST'">
     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-yellow-400 rounded-full">Graphiste</span>
   </div>
-  <div v-else-if="props.rank === 'Live'">
+  <div v-else-if="props.rank === 'STREAMER'">
     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-gray-800 text-xs font-medium bg-purple-400 rounded-full">Streamer</span>
   </div>
   <div v-else>
