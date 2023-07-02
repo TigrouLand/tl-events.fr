38c879f40cc86a0ae01013a6d558b909fa48ffb6
Romitou <romitou@protonmail.com>
2023-07-02 11:41:46 +0200

    :sparkles: Add a discord login page

---
+++ layouts/standalone.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col h-screen w-full">
+    <UpcomingGame />
+    <Disclosure as="nav" class="bg-red-500">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-center">
+          <div class="flex flex-1 items-center justify-center">
+            <img class="h-16 w-auto" src="~/assets/logo.png" alt="">
+          </div>
+        </div>
+      </div>
+    </Disclosure>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { Disclosure } from '@headlessui/vue';
+import UpcomingGame from '~/components/UpcomingGame.vue';
+</script>
---
+++ pages/discordLogin.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex h-full w-full bg-zinc-800 items-center justify-center">
+    <div class="flex-row justify-center w-max-xs w-full">
+      <h1 class="flex justify-center text-2xl text-zinc-200">
+        Connexion en cours
+      </h1>
+      <h1 class="flex justify-center text-lg text-zinc-200">
+        Vous allez être redirigé vers la page de connexion.
+      </h1>
+      <div class="flex justify-center mt-5">
+        <img class="h-16 w-auto" src="~/assets/loading.svg" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script async>
+import { useUserStore } from '~/stores/user';
+
+definePageMeta({
+  layout: 'standalone'
+});
+
+setTimeout(() => {
+  login();
+}, 1000);
+
+const login = async () => {
+  const url = await useUserStore().getLoginUrl();
+  if (!url) {
+    return;
+  }
+  window.location = url;
+};
+</script>
+
+<style scoped>
+
+</style>
