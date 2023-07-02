a748725c85a5f896a23ff433a013f343407714c8
Romitou <romitou@protonmail.com>
2023-07-02 10:47:49 +0200

    :bug: Fix always-true condition

--- components/UpcomingGame.vue
+++ components/UpcomingGame.vue
@@ -1,6 +1,6 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div v-if="upcomingGame?.data" class="bg-zinc-900">
+  <div v-if="upcomingGame?.data?.value" class="bg-zinc-900">
     <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between flex-wrap">
         <div class="w-0 flex-1 flex items-center">
