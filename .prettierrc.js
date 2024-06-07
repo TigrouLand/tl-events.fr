b04124e34c55c12a34aee13216893855426fe0ca
Walidoux <ma.walidkorchi@icloud.com>
2024-06-07 14:38:31 +0100

    refactor(format/lint): separate formatting from eslint + add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  bracketSameLine: true,
+  trailingComma: 'es5',
+  printWidth: 140,
+};
