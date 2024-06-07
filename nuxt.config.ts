a5ceb35900544af34a174c90b366600dc7d2e997
Walidoux <ma.walidkorchi@icloud.com>
2024-06-07 14:16:32 +0100

    refactor(icons): replace fontawesome plugin with nuxt-icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,28 @@
-import eslintPlugin from 'vite-plugin-eslint';
-import { metaTags } from './site.config';
+import { metaTags } from './tools/seo';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
-  css: [
-    '@fortawesome/fontawesome-svg-core/styles.css'
-  ],
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
+  telemetry: false,
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', 'nuxt-icon'],
   app: {
     head: {
       meta: metaTags,
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      links: [
+      link: [
         {
           rel: 'icon',
           type: 'image/x-icon',
-          href: '/favicon.ico'
-        }
-      ]
-    }
+          href: '/favicon.ico',
+        },
+      ],
+    },
   },
-  nitro: {
-    preset: 'cloudflare-pages'
-  },
-  vite: {
-    plugins: [
-      eslintPlugin()
-    ]
-  }
 });
