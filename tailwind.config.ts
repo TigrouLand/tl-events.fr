da53893e6685bd9fbff7a7739438d60927a558e6
Walidoux <ma.walidkorchi@icloud.com>
2026-07-05 01:04:05 +0100

    feat(design): new design system + out of the box tw animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,19 @@
 import type { Config } from 'tailwindcss'
+import animate from 'tailwindcss-animate'
 
 export default {
+  plugins: [animate],
   content: ['./components/**/*.{js,vue,ts}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}'],
   theme: {
     extend: {
+      fontFamily: {
+        inter: 'Inter'
+      },
       colors: {
         primary: '#b23734',
         discord: '#5865f2',
-        twitter: '#1c9cea'
+        mumble: '#1e3a8a',
+        github: '#111827'
       }
     }
   }
