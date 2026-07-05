5a9be74e4040d6a1e1a98145cf34f605c1c26198
Walidoux <ma.walidkorchi@icloud.com>
2026-07-05 01:06:12 +0100

    feat(components): new skeleton for loading states UIs

---
+++ components/ui/skeleton/Skeleton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+
+interface SkeletonProps {
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<SkeletonProps>()
+</script>
+
+<template>
+  <div data-slot="skeleton" :class="cn('bg-muted rounded-md animate-pulse', props.class)" />
+</template>
