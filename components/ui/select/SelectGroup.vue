1898d565bfe7f97dcbe1c5a2f25fa08188d9ae16
Walidoux <ma.walidkorchi@icloud.com>
2026-07-05 01:06:37 +0100

    feat(components): new select for styled item selection

---
+++ components/ui/select/SelectGroup.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { SelectGroupProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { SelectGroup } from 'reka-ui'
+
+const props = defineProps<SelectGroupProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <SelectGroup data-slot="select-group" v-bind="delegatedProps" :class="cn('scroll-my-1 p-1', props.class)">
+    <slot />
+  </SelectGroup>
+</template>
